7fa2b9e6603d9078ee8cf3922fd9cb2a70e56f4f
Matheus Porto <matheus.pereira11@gmail.com>
2018-03-27 11:54:08 -0300

    add devise authentication, seeds and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'Matt', email: 'matt@gmail.com', password: 'foobar123', password_confirmation: 'foobar123')
+User.create!(name: 'Rodolpho', email: 'rodolpho@gmail.com', password: 'testing3523', password_confirmation: 'testing3523')
